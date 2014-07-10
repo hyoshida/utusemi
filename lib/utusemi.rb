6a6169a5f1c929cacb35b6d282f059c497809869
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-10 22:39:12 +0900

    rubocop: Refactor enable method in utusemi.rb

--- lib/utusemi.rb
+++ lib/utusemi.rb
@@ -7,18 +7,16 @@ require 'utusemi/railtie'
 module Utusemi
   class << self
     def enable
+      this = self
       ActiveSupport.on_load(:active_record) do
         require 'utusemi/core'
-
-        ::ActiveRecord::Base.send(:include, Core::InstanceMethods)
-
-        ::ActiveRecord::Relation.send(:prepend, Core::ActiveRecord::QueryMethods)
-        ::ActiveRecord::Relation.send(:prepend, Core::ActiveRecord::Relation)
-        ::ActiveRecord::Base.class_eval { class << self; self; end }.send(:prepend, Core::ActiveRecord::Querying)
-        ::ActiveRecord::Base.class_eval { class << self; self; end }.send(:prepend, Core::ActiveRecord::RelationMethod) if Rails::VERSION::MAJOR == 3
-        ::ActiveRecord::Base.send(:prepend, Core::ActiveRecord::Base)
-        ::ActiveRecord::Associations::HasManyAssociation.send(:prepend, Core::ActiveRecord::Associations)
-        ::ActiveRecord::Base.class_eval { class << self; self; end }.send(:prepend, Core::ActiveRecord::AssociationMethods)
+        # for instance
+        this.include_to_activerecord_base
+        # for model and relation
+        this.prepend_to_activerecord_base
+        this.prepend_to_activerecord_relation
+        this.prepend_to_activerecord_eigenclass
+        this.prepend_to_activerecord_associations_hasmanyassociation
       end
     end
 
@@ -28,6 +26,37 @@ module Utusemi
 
     def configure(&block)
       config.instance_eval(&block)
+    end
+
+    def include_to_activerecord_base
+      ActiveRecord::Base.send(:include, Core::InstanceMethods)
+    end
+
+    def prepend_to_activerecord_base
+      ActiveRecord::Base.send(:prepend, Core::ActiveRecord::Base)
+    end
+
+    def prepend_to_activerecord_relation
+      ActiveRecord::Relation.send(:prepend, Core::ActiveRecord::QueryMethods)
+      ActiveRecord::Relation.send(:prepend, Core::ActiveRecord::Relation)
+    end
+
+    def prepend_to_activerecord_eigenclass
+      activerecord_eigenclass.send(:prepend, Core::ActiveRecord::Querying)
+      # for rails 3.x
+      activerecord_eigenclass.send(:prepend, Core::ActiveRecord::RelationMethod) if Rails::VERSION::MAJOR == 3
+      # for association
+      activerecord_eigenclass.send(:prepend, Core::ActiveRecord::AssociationMethods)
+    end
+
+    def prepend_to_activerecord_associations_hasmanyassociation
+      ActiveRecord::Associations::HasManyAssociation.send(:prepend, Core::ActiveRecord::Associations)
+    end
+
+    private
+
+    def activerecord_eigenclass
+      ActiveRecord::Base.class_eval { class << self; self; end }
     end
   end
 end
