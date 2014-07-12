c1eac1fe5be2b4ed26c13b5bd1262aca03e478a8
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-12 16:40:50 +0900

    Add a test for options argument

--- spec/lib/utusemi/configuration_spec.rb
+++ spec/lib/utusemi/configuration_spec.rb
@@ -1,23 +1,27 @@
 describe Utusemi::Configuration do
+  let(:product) { FactoryGirl.build(:product) }
+
   before do
     Utusemi.configure do
-      map :sample do
+      map :sample do |options|
         name :title
+        caption options[:caption] || :none
       end
     end
     class Product < ActiveRecord::Base; end
   end
 
   subject { Product }
-
   it { should respond_to(:utusemi) }
 
   context 'ActiveRecord::Base#utusemi' do
-    let(:product) { FactoryGirl.build(:product) }
-
     subject { product.utusemi(:sample) }
-
     it { should respond_to(:title) }
     it { should respond_to(:name) }
+  end
+
+  context 'ActiveRecord::Base#utusemi with options' do
+    subject { product.utusemi(:sample, caption: :title) }
+    it { expect(subject.caption).to eq(subject.title) }
   end
 end
