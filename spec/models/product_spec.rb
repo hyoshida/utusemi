dd7b3e1e078b1ea61e20412289a2b4e2a9477bf9
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-12 20:49:24 +0900

    Add tests for ::utusemi method

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -27,4 +27,30 @@ describe Product do
     subject { product.utusemi(:sample, caption: :title) }
     it { expect(subject.caption).to eq(subject.title) }
   end
+
+  describe '::utusemi(type)' do
+    before { FactoryGirl.create(:product, title: 'foobar') }
+    subject { described_class.utusemi(:sample) }
+
+    it '::where by alias column' do
+      expect(subject.where(name: 'foobar').count).to eq(1)
+    end
+
+    it '::order by alias column' do
+      expect { subject.order(:name) }.not_to raise_error
+    end
+
+    it 'call alias column from instance' do
+      expect(subject.first.name).to eq(subject.first.title)
+    end
+  end
+
+  describe '::utusemi(type, options)' do
+    before { FactoryGirl.create(:product, title: 'foobar') }
+    subject { described_class.utusemi(:sample, caption: :title) }
+
+    it 'call alias column from instance' do
+      expect(subject.first.caption).to eq(subject.first.title)
+    end
+  end
 end
