9876ff6a07a3a39bcc9fc5dade12c208e75953ad
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-15 19:05:47 +0900

    Fix problem for options argument in Utusemi::Configuration#map

--- lib/utusemi/configuration.rb
+++ lib/utusemi/configuration.rb
@@ -5,6 +5,7 @@ module Utusemi
     end
 
     def map(name, *args, &block)
+      name = (name || '').to_sym
       return map_get(name, *args) unless block_given?
       options = args.shift if args.first.is_a? Hash
       map_set(name, options, &block)
@@ -13,12 +14,14 @@ module Utusemi
     private
 
     def map_set(name, options, &block)
-      @maps[name.to_sym] = (options || {}).merge(block: block)
+      @maps[name] = {}
+      @maps[name].update(options) if options
+      @maps[name].update(block: block)
     end
 
     def map_get(name, *args)
-      map = @maps[name.to_sym].try(:dup) if name
-      block = map.delete(:block) if map
+      map = @maps[name].try(:dup) || {}
+      block = map.delete(:block)
       definition = Definition.new(map)
       definition.exec(*args, &block)
     end
