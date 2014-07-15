357154e816e91d6ce88fa292cb9cf7df273a2c4a
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-15 18:22:26 +0900

    Add tests for times option of ActiveRecord::Base::utusemi

--- spec/dummy/db/migrate/20140710120737_create_products.rb
+++ spec/dummy/db/migrate/20140710120737_create_products.rb
@@ -2,6 +2,9 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
+      t.string :description1
+      t.string :description2
+      t.string :description3
       t.timestamps
     end
   end
--- spec/lib/utusemi/configuration_spec.rb
+++ spec/lib/utusemi/configuration_spec.rb
@@ -1,2 +1,24 @@
 describe Utusemi::Configuration do
+  describe '"times" option' do
+    let!(:product_one) { FactoryGirl.create(:product, description1: nil, description2: nil, description3: nil) }
+    let!(:product_two) { FactoryGirl.create(:product, description1: 'foo', description2: 'bar', description3: 'hoge') }
+    let!(:product_three) { FactoryGirl.create(:product, description1: 'foo', description2: 'bar', description3: nil) }
+
+    before do
+      Utusemi.configure do
+        map(:sample) { |options| description "description#{options[:index]}" }
+      end
+    end
+
+    subject { Product.utusemi(:sample, times: 3) }
+
+    it 'found a record that all description are nil' do
+      expect(subject.where(description: nil).count).to eq(1)
+    end
+
+    it 'found a record that all description are not nil' do
+      pending 'Rails 3 is not supported' if Rails::VERSION::MAJOR == 3
+      expect(subject.where.not(description: nil).count).to eq(1)
+    end
+  end
 end
