95ba1e57ca105be995b462408f5ad1c747fd1574
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-20 17:04:28 +0900

    Fix syntax in product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :product do
     title 'My favorite product'
 
-    trait :with_stock
-    stocks { FactoryGirl.build_list(:stock, 1) }
+    trait :with_stock do
+      stocks { build_list(:stock, 1) }
+    end
   end
 end
